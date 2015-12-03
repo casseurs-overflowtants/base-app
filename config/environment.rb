d069e5fb8b9a1e882e899a767eb05fafe2c64917
Julien Gamba <jgamba@aius.u-strasbg.fr>
2015-12-03 23:10:16 +0100

    [Fix] logger config

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,4 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+Rails.logger = Le.new('0f1cef8d-b05f-363c-8b75-4bb23c4a9374', :debug => true, :local => true)
