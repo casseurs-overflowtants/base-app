8744c24aeeebf50e0a56773ec059559428c8b238
Benjamin Chetioui <chetioui.benjamin@gmail.com>
2015-12-04 07:28:45 +0100

    [Addition] Band.

---
+++ app/assets/javascripts/ConnectionAPI.js
@@ -0,0 +1,9 @@
+function getConnectionPage() {
+    alert(4);
+    if (document.getElementById('connectionTab').classname == 'hidden') {
+        document.getElementById('connectionTab').classname = 'visible');
+    }
+    else {
+        document.getElementById('connectionTab').classname = 'hidden';
+    }
+}
