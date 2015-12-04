5adae35d5419a9fb144c777b3182fe64af2ea8d3
Benjamin Chetioui <chetioui.benjamin@gmail.com>
2015-12-04 07:47:54 +0100

    [LOLCOMMIT]

--- app/assets/javascripts/ConnectionAPI.js
+++
@@ -1,9 +0,0 @@
-function getConnectionPage() {
-    alert(4);
-    if (document.getElementById('connectionTab').classname == 'hidden') {
-        document.getElementById('connectionTab').classname = 'visible');
-    }
-    else {
-        document.getElementById('connectionTab').classname = 'hidden';
-    }
-}
